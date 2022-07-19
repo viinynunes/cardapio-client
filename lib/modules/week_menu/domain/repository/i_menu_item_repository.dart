40fe648cddb18ee8454f3204e770bbb24a47568b
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-07-19 14:40:33 -0300

    Changed wrong import of menu item to the correct import from domain/entities

--- lib/modules/week_menu/domain/repository/i_menu_item_repository.dart
+++ lib/modules/week_menu/domain/repository/i_menu_item_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
 
 import '../../errors/get_menu_by_day_error.dart';
+import '../entities/menu_item.dart';
 
 abstract class IMenuItemRepository {
   Either<GetMenuByDayError, List<MenuItem>> getMenuListByDay(DateTime dateTime);
