b353d6baeea17f19c1d9a50826c25f60e10156fd
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-07-19 15:33:55 -0300

    Renamed class to avoid a conflict with a material import

---
+++ lib/modules/week_menu/domain/entities/item_menu.dart
@@ -0,0 +1,7 @@
+class ItemMenu {
+  String name;
+  String description;
+  String imgUrl;
+
+  ItemMenu(this.name, this.description, this.imgUrl);
+}
--- lib/modules/week_menu/domain/entities/menu_item.dart
+++
@@ -1,7 +0,0 @@
-class MenuItem {
-  String name;
-  String description;
-  String imgUrl;
-
-  MenuItem(this.name, this.description, this.imgUrl);
-}
