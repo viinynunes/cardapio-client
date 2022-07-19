745fa819ab6c68c9fba7020e1db7c13ce09c292b
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-07-19 16:44:49 -0300

    Mocking repository

--- lib/modules/week_menu/domain/repository/mock_rep_impl.dart
+++ lib/modules/week_menu/domain/repository/mock_rep_impl.dart
@@ -1,9 +1,17 @@
-import '../../modules/week_menu/domain/entities/item_menu.dart';
+import 'package:cardapio/modules/week_menu/domain/entities/item_menu.dart';
 
-class WeekMenuController {
-  List<ItemMenu> menuList = [];
+import 'package:cardapio/modules/week_menu/errors/get_menu_error.dart';
 
-  Future getList() async {
+import 'package:dartz/dartz.dart';
+
+import 'i_menu_item_repository.dart';
+
+class MockRepImpl implements IItemMenuRepository {
+  @override
+  Future<Either<GetMenuError, List<ItemMenu>>> getItemMenuByDay(
+      DateTime dateTime) async {
+    List<ItemMenu> menuList = [];
+
     for (int i = 1; i <= 100; i++) {
       menuList.add(
         ItemMenu(
@@ -14,5 +22,7 @@ class WeekMenuController {
                 'prato-feito.jpg?itok=klqGuxxN&c=33313733cdad61e4bd51beabb4a84531'),
       );
     }
+
+    return Right(menuList);
   }
 }
