05dce3ad986183c807bd86b57fcb4f4c117cd268
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-07-20 20:48:53 -0300

    Implemented bloc manager

--- lib/android/week_menu/week_menu_day_home.dart
+++ lib/android/week_menu/week_menu_day_home.dart
@@ -1,24 +1,31 @@
-import 'package:cardapio/android/controllers/i_week_menu_controller.dart';
+import 'package:cardapio/android/controllers/bloc/events/events.dart';
+import 'package:cardapio/android/controllers/bloc/states/states.dart';
+import 'package:cardapio/android/controllers/bloc/week_menu_bloc.dart';
 import 'package:cardapio/android/week_menu/week_menu_day_item.dart';
-import 'package:cardapio/modules/week_menu/domain/entities/item_menu.dart';
 import 'package:flutter/material.dart';
 
 class WeekMenuDayHome extends StatefulWidget {
-  const WeekMenuDayHome({Key? key, required this.controller}) : super(key: key);
-
-  final IWeekMenuController controller;
+  const WeekMenuDayHome({Key? key}) : super(key: key);
 
   @override
   State<WeekMenuDayHome> createState() => _WeekMenuDayHomeState();
 }
 
 class _WeekMenuDayHomeState extends State<WeekMenuDayHome> {
-  late List<ItemMenu> menuItemList;
+  late final WeekMenuBlock bloc;
 
   @override
-  void initState() async {
+  void initState() {
     super.initState();
-    menuItemList = await widget.controller.getItemMenuList();
+
+    bloc = WeekMenuBlock();
+    bloc.add(GetListByDayEvent(DateTime.now()));
+  }
+
+  @override
+  void dispose() {
+    bloc.close();
+    super.dispose();
   }
 
   @override
@@ -35,11 +42,11 @@ class _WeekMenuDayHomeState extends State<WeekMenuDayHome> {
               alignment: Alignment.center,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('HOJE: '),
+                children: const [
+                  Text('HOJE: '),
                   Text(
-                    widget.controller.getWeekDay(DateTime.now()),
-                    style: const TextStyle(fontSize: 16),
+                    'AAAAA',
+                    style: TextStyle(fontSize: 16),
                   ),
                 ],
               )),
@@ -50,49 +57,65 @@ class _WeekMenuDayHomeState extends State<WeekMenuDayHome> {
           children: [
             SizedBox(
               height: size.height * 0.9,
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  childAspectRatio: 1,
-                  maxCrossAxisExtent: 300,
-                ),
-                itemCount: menuItemList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  var item = menuItemList[index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => WeekMenuDayItem(menuItem: item),
+              child: StreamBuilder<MenuState>(
+                stream: bloc.stream,
+                builder: (context, snap) {
+                  if (!snap.hasData) {
+                    return const Center(
+                      child: Text('Error'),
+                    );
+                  }
+
+                  final menuItemList = snap.data!.menuByDayList;
+
+                  return GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      childAspectRatio: 1,
+                      maxCrossAxisExtent: 300,
+                    ),
+                    itemCount: menuItemList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var item = menuItemList[index];
+
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  WeekMenuDayItem(menuItem: item),
+                            ),
+                          );
+                        },
+                        child: Stack(
+                          children: [
+                            Card(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(item.imgUrl),
+                                    fit: BoxFit.cover,
+                                    colorFilter: const ColorFilter.mode(
+                                        Colors.black26, BlendMode.darken),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(8),
+                              alignment: Alignment.bottomCenter,
+                              child: Text(
+                                item.name,
+                                style: const TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
                         ),
                       );
                     },
-                    child: Stack(
-                      children: [
-                        Card(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(item.imgUrl),
-                                fit: BoxFit.cover,
-                                colorFilter: const ColorFilter.mode(
-                                    Colors.black26, BlendMode.darken),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(8),
-                          alignment: Alignment.bottomCenter,
-                          child: Text(
-                            item.name,
-                            style: const TextStyle(
-                                fontSize: 20, color: Colors.white),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
-                    ),
                   );
                 },
               ),
