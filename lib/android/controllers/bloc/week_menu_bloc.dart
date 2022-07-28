4203acc5f6fee279382a5e588b3ad18da45e7c26
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-07-28 16:13:14 -0300

    Removed if to check if the result is left or right to emit states directly on fold

--- lib/android/controllers/bloc/week_menu_bloc.dart
+++ lib/android/controllers/bloc/week_menu_bloc.dart
@@ -3,7 +3,6 @@ import 'package:cardapio/android/controllers/bloc/events/week_menu_events.dart';
 import 'package:cardapio/android/controllers/bloc/states/week_menu_states.dart';
 import 'package:cardapio/modules/week_menu/domain/repository/mock_rep_impl.dart';
 import 'package:cardapio/modules/week_menu/domain/usecases/impl/menu_item_usecase_impl.dart';
-import 'package:cardapio/modules/week_menu/errors/get_menu_error.dart';
 
 class WeekMenuBlock extends Bloc<MenuEvent, MenuState> {
   final usecase = ItemMenuUsecaseImpl(MockRepImpl());
@@ -14,14 +13,8 @@ class WeekMenuBlock extends Bloc<MenuEvent, MenuState> {
 
       final result = await usecase.getItemMenuByDay(event.weekday);
 
-      if (result.isLeft()) {
-        emit(MenuErrorState(
-            error: result.fold((l) => l, (r) => GetMenuError(''))));
-      } else {
-        emit(MenuSuccessState(
-            menuByDayList: result.fold((l) => [], (r) => r),
-            error: GetMenuError('')));
-      }
+      result.fold((l) => emit(MenuErrorState(error: l)),
+          (r) => emit(MenuSuccessState(menuByDayList: r)));
     });
   }
 }
