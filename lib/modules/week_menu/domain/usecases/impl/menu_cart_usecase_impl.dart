814ba6854d2c2768c36cb9ccdf1a287a557c31fa
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-07-29 14:27:58 -0300

    Implementation of menu cart usecase

---
+++ lib/modules/week_menu/domain/usecases/impl/menu_cart_usecase_impl.dart
@@ -0,0 +1,29 @@
+import 'package:cardapio/modules/week_menu/domain/entities/item_menu.dart';
+import 'package:cardapio/modules/week_menu/domain/repository/i_menu_cart_repository.dart';
+
+import 'package:cardapio/modules/week_menu/errors/get_menu_error.dart';
+
+import 'package:dartz/dartz.dart';
+
+import '../i_menu_cart_usecase.dart';
+
+class MenuCartUsecaseImpl implements IMenuCartUsecase {
+  final IMenuCartRepository _repository;
+
+  MenuCartUsecaseImpl(this._repository);
+
+  @override
+  Future<Either<MenuCartError, bool>> removeItemFromCart(
+      ItemMenu itemMenu) async {
+    if (itemMenu.name.isEmpty) {
+      return Left(MenuCartError('Invalid menu item'));
+    }
+
+    return await _repository.removeItemFromCart(itemMenu);
+  }
+
+  @override
+  Future<Either<MenuCartError, List<ItemMenu>>> getMenuCartList() async {
+    return await _repository.getMenuCartList();
+  }
+}
