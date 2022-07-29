9e5d6efea729f3a81bcaf04c4d721438878ce0bc
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-07-29 14:27:41 -0300

    Interfaces of menu cart usecase and repository

---
+++ lib/modules/week_menu/domain/usecases/i_menu_cart_usecase.dart
@@ -0,0 +1,9 @@
+import 'package:cardapio/modules/week_menu/domain/entities/item_menu.dart';
+import 'package:cardapio/modules/week_menu/errors/get_menu_error.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class IMenuCartUsecase {
+  Future<Either<MenuCartError, bool>> removeItemFromCart(ItemMenu itemMenu);
+
+  Future<Either<MenuCartError, List<ItemMenu>>> getMenuCartList();
+}
