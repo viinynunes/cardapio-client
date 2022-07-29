b7632c15425b145b3bab002e826d2e98621c8f8e
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-07-29 15:06:55 -0300

    Menu cart page with bloc

--- lib/android/order/menu_cart.dart
+++ lib/android/order/menu_cart.dart
@@ -1,5 +1,8 @@
-import 'package:cardapio/android/controllers/menu_cart_controller.dart';
+import 'package:cardapio/android/controllers/bloc/events/menu_cart_events.dart';
+import 'package:cardapio/android/controllers/bloc/menu_cart_bloc.dart';
+import 'package:cardapio/android/controllers/bloc/states/menu_cart_states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MenuCart extends StatefulWidget {
   const MenuCart({Key? key}) : super(key: key);
@@ -9,13 +12,14 @@ class MenuCart extends StatefulWidget {
 }
 
 class _MenuCartState extends State<MenuCart> {
-  final controller = MenuCartController();
+  late final MenuCartBloc bloc;
 
   @override
   void initState() {
     super.initState();
 
-    controller.getList();
+    bloc = MenuCartBloc();
+    bloc.add(GetMenuCartList());
   }
 
   @override
@@ -23,96 +27,129 @@ class _MenuCartState extends State<MenuCart> {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Carrinho'),
-        centerTitle: true,
-        actions: [
-          GestureDetector(
-            onTap: (){
-
-            },
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Row(
-                children: const [
-                  Text(
-                    'Confirmar pedido',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Icon(Icons.save),
-                ],
+        appBar: AppBar(
+          title: const Text('Carrinho'),
+          centerTitle: true,
+          actions: [
+            GestureDetector(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  children: const [
+                    Text(
+                      'Confirmar pedido',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(Icons.save),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-      body: controller.menuItemCartList.isNotEmpty
-          ? ListView.builder(
-              itemCount: controller.menuItemCartList.length,
-              itemBuilder: (context, index) {
-                var item = controller.menuItemCartList[index];
-                return Card(
-                  child: Container(
-                    height: size.height * 0.2,
-                    margin: const EdgeInsets.all(16),
-                    child: Row(
-                      children: [
-                        Flexible(
+          ],
+        ),
+        body: BlocBuilder(
+          bloc: bloc,
+          builder: (context, state) {
+            if (state is MenuCartLoadingState) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (state is MenuCartErrorState) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.error_outline,
+                      size: 100,
+                    ),
+                    Text(
+                      state.error.message,
+                      style: const TextStyle(fontSize: 50),
+                    ),
+                  ],
+                ),
+              );
+            }
+
+            if (state is MenuCartSuccessState) {
+              final menuItemCartList = state.menuItemCartList;
+
+              return menuItemCartList.isEmpty
+                  ? ListView.builder(
+                      itemCount: menuItemCartList.length,
+                      itemBuilder: (context, index) {
+                        var item = menuItemCartList[index];
+
+                        return Card(
                           child: Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                image: NetworkImage(item.imgUrl),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Flexible(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            height: size.height * 0.2,
+                            margin: const EdgeInsets.all(16),
+                            child: Row(
                               children: [
-                                Text(
-                                  item.name,
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(fontSize: 22),
+                                Flexible(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: NetworkImage(item.imgUrl),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
                                 ),
-                                ElevatedButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        controller.removeMenuItem(item);
-                                      });
-                                    },
-                                    child: const Text('Remover Item'))
+                                const SizedBox(
+                                  width: 15,
+                                ),
+                                Flexible(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          item.name,
+                                          textAlign: TextAlign.center,
+                                          style: const TextStyle(fontSize: 22),
+                                        ),
+                                        ElevatedButton(
+                                            onPressed: () {
+                                              bloc.add(
+                                                  RemoveItemMenuFromCart(item));
+                                            },
+                                            child: const Text('Remover Item'))
+                                      ],
+                                    ),
+                                  ),
+                                ),
                               ],
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            )
-          : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Icon(
-                    Icons.shopping_cart_outlined,
-                    size: 100,
-                  ),
-                  Text(
-                    'Carrinho Vazio',
-                    style: TextStyle(fontSize: 50),
-                  ),
-                ],
-              ),
-            ),
-    );
+                        );
+                      },
+                    )
+                  : Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.shopping_cart_outlined,
+                            size: 100,
+                          ),
+                          Text(
+                            'Carrinho Vazio',
+                            style: TextStyle(fontSize: 50),
+                          ),
+                        ],
+                      ),
+                    );
+            }
+
+            return Container();
+          },
+        ));
   }
 }
