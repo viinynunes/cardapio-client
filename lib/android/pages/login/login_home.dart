57597c640dc870a22b22501513374c05308c67f3
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-09 18:36:30 -0300

    Added support to bloc and modular

--- lib/android/pages/login/login_home.dart
+++ lib/android/pages/login/login_home.dart
@@ -1,4 +1,8 @@
+import 'package:cardapio/android/controllers/bloc/events/login_events.dart';
+import 'package:cardapio/android/controllers/bloc/login_bloc.dart';
+import 'package:cardapio/android/controllers/bloc/states/login_states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class LoginHome extends StatefulWidget {
@@ -9,7 +13,12 @@ class LoginHome extends StatefulWidget {
 }
 
 class _LoginHomeState extends State<LoginHome> {
+  final bloc = Modular.get<LoginBloc>();
+
   final formKey = GlobalKey<FormState>();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +43,7 @@ class _LoginHomeState extends State<LoginHome> {
                       color: Colors.red,
                     ),
                     TextFormField(
+                      controller: _emailController,
                       decoration: const InputDecoration(hintText: 'Email'),
                       keyboardType: TextInputType.emailAddress,
                       textInputAction: TextInputAction.next,
@@ -48,11 +58,12 @@ class _LoginHomeState extends State<LoginHome> {
                       height: 20,
                     ),
                     TextFormField(
+                      controller: _passwordController,
                       decoration: const InputDecoration(hintText: 'Senha'),
                       obscureText: true,
                       validator: (text) {
-                        if (text!.length < 4) {
-                          return 'A senha deve ter mais de 4 caracteres';
+                        if (text!.length < 6) {
+                          return 'A senha deve ter mais de 6 caracteres';
                         }
                         return null;
                       },
@@ -60,21 +71,63 @@ class _LoginHomeState extends State<LoginHome> {
                     const SizedBox(
                       height: 20,
                     ),
-                    Container(
-                      width: size.width * 0.9,
-                      height: size.height * 0.05,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(200),
-                      ),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            Modular.to.navigate('/android-home/');
-                          }
-                        },
-                        child: const Text('Entrar'),
-                      ),
-                    )
+                    Column(
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              final email = _emailController.text;
+                              final password = _passwordController.text;
+
+                              bloc.add(UserLoginEvent(email, password));
+                            }
+                          },
+                          child: const Text('Entrar'),
+                        ),
+                        Container(
+                          width: size.width * 0.9,
+                          height: size.height * 0.05,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(200),
+                          ),
+                          child: BlocListener<LoginBloc, LoginStates>(
+                            bloc: bloc,
+                            listener: (context, state) {
+                              if (state is LoginErrorState) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    backgroundColor: Colors.red,
+                                    content: Text(state.message),
+                                  ),
+                                );
+                              }
+
+                              if (state is LoginSuccessState) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    backgroundColor: Colors.blue,
+                                    content: Text('Login Efetuado com sucesso'),
+                                  ),
+                                );
+                                Modular.to.navigate('/android-home');
+                              }
+                            },
+                            child: BlocBuilder<LoginBloc, LoginStates>(
+                              bloc: bloc,
+                              builder: (context, state) {
+                                if (state is LoginLoadingState) {
+                                  return const Center(
+                                    child: CircularProgressIndicator(),
+                                  );
+                                }
+
+                                return Container();
+                              },
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ],
                 ),
               ),
