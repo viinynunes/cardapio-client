ada8c46e2ec93068b2fb2a226b2c9d03fa84d915
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-09 18:34:48 -0300

    Implementation of login usecase

---
+++ lib/modules/login/domain/usecases/impl/login_usecase_impl.dart
@@ -0,0 +1,25 @@
+import 'package:cardapio/modules/login/domain/entities/user.dart';
+import 'package:cardapio/modules/login/domain/repositories/i_login_repository.dart';
+import 'package:cardapio/modules/login/domain/usecases/i_login_usecase.dart';
+import 'package:cardapio/modules/week_menu/errors/errors.dart';
+import 'package:dartz/dartz.dart';
+import 'package:string_validator/string_validator.dart';
+
+class LoginUsecaseImpl implements ILoginUsecase {
+  final ILoginRepository _repository;
+
+  LoginUsecaseImpl(this._repository);
+
+  @override
+  Future<Either<LoginError, User>> login(String email, String password) async {
+    if (!isEmail(email)) {
+      return Left(LoginError('Invalid email'));
+    }
+
+    if (password.length < 6) {
+      return Left(LoginError('Password cannot be less then 6 characters'));
+    }
+
+    return _repository.login(email, password);
+  }
+}
