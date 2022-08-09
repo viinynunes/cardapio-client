8a05db26df39295574dd3b669979b40bd5df82b2
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-09 18:34:32 -0300

    Login usecase and repository interface

---
+++ lib/modules/login/domain/repositories/i_login_repository.dart
@@ -0,0 +1,7 @@
+import 'package:cardapio/modules/login/domain/entities/user.dart';
+import 'package:cardapio/modules/week_menu/errors/errors.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class ILoginRepository {
+  Future<Either<LoginError, User>> login(String email, String password);
+}
