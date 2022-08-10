aa0e0ac43e838d4e59ef6a1ebe7c414efc224a5b
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-10 16:51:29 -0300

    Order usecase and repository interface

---
+++ lib/modules/order/domain/repositories/i_order_repository.dart
@@ -0,0 +1,12 @@
+import 'package:cardapio/modules/errors/errors.dart';
+import 'package:dartz/dartz.dart';
+
+import '../entities/order.dart' as order;
+
+
+
+abstract class IOrderRepository {
+  Future<Either<OrderError, order.Order>> create(order.Order order);
+
+  Future<Either<OrderError, order.Order>> cancel(order.Order order);
+}
---
+++ lib/modules/order/domain/usecases/i_order_usecase.dart
@@ -0,0 +1,10 @@
+import 'package:cardapio/modules/errors/errors.dart';
+import 'package:dartz/dartz.dart';
+
+import '../entities/order.dart' as order;
+
+abstract class IOrderUsecase {
+  Future<Either<OrderError, order.Order>> create(order.Order order);
+
+  Future<Either<OrderError, order.Order>> cancel(order.Order order);
+}
