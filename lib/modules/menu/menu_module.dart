41e05da10e353965bc5e1a396cdc89a4c4991a1e
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-12 16:29:37 -0300

    Renamed Mock

--- lib/modules/menu/menu_module.dart
+++ lib/modules/menu/menu_module.dart
@@ -1,29 +1,27 @@
-import 'package:cardapio/modules/menu/presenter/bloc/menu_by_day_bloc.dart';
-import 'package:cardapio/modules/menu/presenter/bloc/menu_by_day_item_bloc.dart';
-import 'package:cardapio/modules/menu/presenter/bloc/days_of_week_bloc.dart';
-import 'package:cardapio/modules/menu/presenter/pages/days_of_week_page.dart';
-import 'package:cardapio/modules/menu/presenter/pages/menu_by_day_page.dart';
-import 'package:cardapio/modules/menu/presenter/pages/menu_by_day_item_page.dart';
 import 'package:cardapio/modules/menu/domain/repository/mock_rep_impl.dart';
 import 'package:cardapio/modules/menu/domain/usecases/impl/days_of_week_usecase_impl.dart';
-import 'package:cardapio/modules/order/domain/usecases/impl/menu_cart_usecase_impl.dart';
 import 'package:cardapio/modules/menu/domain/usecases/impl/menu_item_usecase_impl.dart';
+import 'package:cardapio/modules/menu/presenter/bloc/days_of_week_bloc.dart';
+import 'package:cardapio/modules/menu/presenter/bloc/menu_by_day_bloc.dart';
+import 'package:cardapio/modules/menu/presenter/bloc/menu_by_day_item_bloc.dart';
+import 'package:cardapio/modules/menu/presenter/pages/days_of_week_page.dart';
+import 'package:cardapio/modules/menu/presenter/pages/menu_by_day_item_page.dart';
+import 'package:cardapio/modules/menu/presenter/pages/menu_by_day_page.dart';
+import 'package:cardapio/modules/order/domain/usecases/impl/menu_cart_usecase_impl.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class MenuModule extends Module {
   @override
   List<Bind> get binds => [
-    Bind.factory((i) => DaysOfWeekUsecaseImpl()),
-    Bind.singleton((i) => DaysOfWeekBloc(i())),
-
-    Bind.factory((i) => MockRepImpl()),
-    Bind.factory((i) => ItemMenuUsecaseImpl(i())),
-    Bind.singleton((i) => MenuByDayBloc(i())),
-
-    Bind.factory((i) => MockRepMenuCartImpl()),
-    Bind.factory((i) => MenuCartUsecaseImpl(i())),
-    Bind.singleton((i) => MenuByDayItemBloc(i())),
-  ];
+        Bind.factory((i) => DaysOfWeekUsecaseImpl()),
+        Bind.singleton((i) => DaysOfWeekBloc(i())),
+        Bind.factory((i) => MockRepImpl()),
+        Bind.factory((i) => ItemMenuUsecaseImpl(i())),
+        Bind.singleton((i) => MenuByDayBloc(i())),
+        Bind.factory((i) => MockMenuCartRepositoryImpl()),
+        Bind.factory((i) => MenuCartUsecaseImpl(i())),
+        Bind.singleton((i) => MenuByDayItemBloc(i())),
+      ];
 
   @override
   List<ModularRoute> get routes => [
