36602c1db5a49afbe73a133675e7e6eaa124c5f4
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-12 16:12:49 -0300

    Added clear menu cart function menu cart usecase and repository

--- lib/modules/order/domain/repositories/i_menu_cart_repository.dart
+++ lib/modules/order/domain/repositories/i_menu_cart_repository.dart
@@ -4,10 +4,11 @@ import 'package:dartz/dartz.dart';
 import '../../../errors/errors.dart';
 
 abstract class IMenuCartRepository {
-
   Future<Either<MenuCartError, ItemMenu>> addItemToCart(ItemMenu itemMenu);
 
   Future<Either<MenuCartError, bool>> removeItemFromCart(ItemMenu itemMenu);
+
+  Future<Either<MenuCartError, bool>> clearMenuCartList();
 
   Future<Either<MenuCartError, List<ItemMenu>>> getMenuCartList();
 }
