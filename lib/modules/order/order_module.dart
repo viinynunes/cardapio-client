67d567c7c0293d38ff902e75a4d63a34299c1f9a
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-15 20:53:53 -0300

    Added Cart Bloc to bind

--- lib/modules/order/order_module.dart
+++ lib/modules/order/order_module.dart
@@ -1,5 +1,8 @@
+import 'package:cardapio/modules/cart/presenter/bloc/cart_bloc.dart';
 import 'package:cardapio/modules/login/domain/repositories/mock_login_repository.dart';
 import 'package:cardapio/modules/login/domain/usecases/impl/login_usecase_impl.dart';
+import 'package:cardapio/modules/order/domain/usecases/impl/order_usecase_impl.dart';
+import 'package:cardapio/modules/order/infra/repositories/mockOrderRepositoryImpl.dart';
 import 'package:cardapio/modules/order/presenter/bloc/order_bloc.dart';
 import 'package:cardapio/modules/order/presenter/pages/personal_orders.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -14,6 +17,9 @@ class OrderModule extends Module {
         Bind.factory((i) => MockMenuCartRepositoryImpl()),
         Bind.factory((i) => CartUsecaseImpl(i())),
         Bind.factory((i) => LoginUsecaseImpl(i())),
+        Bind.factory((i) => MockOrderRepositoryImpl()),
+        Bind.factory((i) => OrderUsecaseImpl(i())),
+        Bind.factory((i) => CartBloc(i())),
         Bind.singleton((i) => OrderBloc(i(), i(), i(), i()))
       ];
 
