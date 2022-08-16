76421152569b30a716092cb2bf3efc4119d05820
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-15 21:08:19 -0300

    Order events added

--- lib/modules/order/presenter/bloc/events/order_events.dart
+++ lib/modules/order/presenter/bloc/events/order_events.dart
@@ -1,3 +1,13 @@
+import '../../../domain/entities/order.dart';
+
 abstract class OrderEvents {}
 
 class SendOrderEvent extends OrderEvents {}
+
+class GetOrderEvent extends OrderEvents {}
+
+class CancelOrderEvent extends OrderEvents {
+  final Order order;
+
+  CancelOrderEvent(this.order);
+}
