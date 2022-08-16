cdc5df894f6b73d00ca5a5882d23d277426e0ebd
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-15 21:09:10 -0300

    Handling with CancelOrderEvent

--- lib/modules/order/presenter/bloc/order_bloc.dart
+++ lib/modules/order/presenter/bloc/order_bloc.dart
@@ -1,15 +1,15 @@
 import 'package:bloc/bloc.dart';
 import 'package:cardapio/modules/errors/errors.dart';
 
+import '../../../cart/domain/usecase/i_cart_usecase.dart';
+import '../../../cart/presenter/bloc/cart_bloc.dart';
 import '../../../login/domain/entities/user.dart';
 import '../../../login/domain/usecases/i_login_usecase.dart';
 import '../../../menu/domain/entities/item_menu.dart';
 import '../../domain/entities/enums/order_status_enum.dart';
 import '../../domain/entities/order.dart';
-import '../../../cart/domain/usecase/i_cart_usecase.dart';
 import '../../domain/usecases/i_order_usecase.dart';
 import 'events/order_events.dart';
-import '../../../cart/presenter/bloc/cart_bloc.dart';
 import 'states/order_states.dart';
 
 class OrderBloc extends Bloc<OrderEvents, OrderStates> {
@@ -18,8 +18,8 @@ class OrderBloc extends Bloc<OrderEvents, OrderStates> {
   final IOrderUsecase orderUsecase;
   final CartBloc menuCartBLoc;
 
-  OrderBloc(this.cartUsecase, this.loginUsecase, this.orderUsecase,
-      this.menuCartBLoc)
+  OrderBloc(
+      this.cartUsecase, this.loginUsecase, this.orderUsecase, this.menuCartBLoc)
       : super(OrderIdleState()) {
     on<SendOrderEvent>((event, emit) async {
       emit(OrderLoadingState());
@@ -47,13 +47,22 @@ class OrderBloc extends Bloc<OrderEvents, OrderStates> {
 
         final orderResult = await orderUsecase.create(order);
 
-        orderResult.fold(
-            (l) => emit(OrderErrorState(l)), (r) => emit(OrderSuccessState(r)));
+        orderResult.fold((l) => emit(OrderErrorState(l)),
+            (r) => emit(OrderSendOrderSuccessState(r)));
 
         await cartUsecase.clearMenuCartList();
       } else {
         emit(OrderErrorState(OrderError('')));
       }
+    });
+
+    on<CancelOrderEvent>((event, emit) async {
+      emit(OrderLoadingState());
+
+      final result = await orderUsecase.cancel(event.order);
+
+      result.fold(
+          (l) => emit(OrderErrorState(l)), (r) => emit(OrderSuccessState()));
     });
   }
 }
