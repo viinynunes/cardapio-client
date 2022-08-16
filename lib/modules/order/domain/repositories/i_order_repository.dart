4afc7a9fd68830393fe090f76a1d7c29c4a65d60
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-15 21:16:38 -0300

    GetOrders contract and his implementation of repository

--- lib/modules/order/domain/repositories/i_order_repository.dart
+++ lib/modules/order/domain/repositories/i_order_repository.dart
@@ -1,12 +1,13 @@
 import 'package:cardapio/modules/errors/errors.dart';
+import 'package:cardapio/modules/login/domain/entities/user.dart';
 import 'package:dartz/dartz.dart';
 
 import '../entities/order.dart' as order;
-
-
 
 abstract class IOrderRepository {
   Future<Either<OrderError, order.Order>> create(order.Order order);
 
   Future<Either<OrderError, order.Order>> cancel(order.Order order);
+
+  Future<Either<OrderError, List<order.Order>>> getOrders(User user);
 }
--- lib/modules/order/infra/repositories/mockOrderRepositoryImpl.dart
+++ lib/modules/order/infra/repositories/mockOrderRepositoryImpl.dart
@@ -3,6 +3,7 @@ import 'package:cardapio/modules/order/domain/entities/enums/order_status_enum.d
 import 'package:cardapio/modules/order/domain/repositories/i_order_repository.dart';
 import 'package:dartz/dartz.dart';
 
+import '../../../login/domain/entities/user.dart';
 import '../../domain/entities/order.dart' as order;
 
 class MockOrderRepositoryImpl implements IOrderRepository {
@@ -21,5 +22,43 @@ class MockOrderRepositoryImpl implements IOrderRepository {
 
     //return Left(OrderError('Order Expired'));
     return Right(order);
+  }
+
+  @override
+  Future<Either<OrderError, List<order.Order>>> getOrders(User user) async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    return Right([
+      order.Order(
+          id: 'id',
+          user: user,
+          menuList: [],
+          registrationDate: DateTime.now(),
+          status: OrderStatus.open),
+      order.Order(
+          id: 'id',
+          user: user,
+          menuList: [],
+          registrationDate: DateTime.now(),
+          status: OrderStatus.confirmed),
+      order.Order(
+          id: 'id',
+          user: user,
+          menuList: [],
+          registrationDate: DateTime.now(),
+          status: OrderStatus.cancelled),
+      order.Order(
+          id: 'id',
+          user: user,
+          menuList: [],
+          registrationDate: DateTime.now(),
+          status: OrderStatus.closed),
+      order.Order(
+          id: 'id',
+          user: user,
+          menuList: [],
+          registrationDate: DateTime.now(),
+          status: OrderStatus.closed),
+    ]);
   }
 }
