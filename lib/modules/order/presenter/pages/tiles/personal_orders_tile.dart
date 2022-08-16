dc8d1517e8555c61813d146c6be4d966af691211
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-16 11:03:38 -0300

    Requiring function onTap on Gesture detector

--- lib/modules/order/presenter/pages/tiles/personal_orders_tile.dart
+++ lib/modules/order/presenter/pages/tiles/personal_orders_tile.dart
@@ -4,9 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class PersonalOrdersTile extends StatelessWidget {
-  const PersonalOrdersTile({Key? key, required this.order}) : super(key: key);
+  const PersonalOrdersTile({Key? key, required this.order, required this.onTap})
+      : super(key: key);
 
   final Order order;
+  final VoidCallback onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -19,38 +21,41 @@ class PersonalOrdersTile extends StatelessWidget {
         color: Colors.grey.withOpacity(0.3),
         borderRadius: BorderRadius.circular(16),
       ),
-      child: Card(
-        child: Row(
-          children: [
-            Flexible(
-              flex: 1,
-              fit: FlexFit.tight,
-              child: Text(
-                dateFormat.format(order.registrationDate),
-                textAlign: TextAlign.center,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Card(
+          child: Row(
+            children: [
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Text(
+                  dateFormat.format(order.registrationDate),
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-            Flexible(
-              flex: 3,
-              fit: FlexFit.tight,
-              child: Text(
-                order.menuList.first.name,
-                textAlign: TextAlign.center,
+              Flexible(
+                flex: 3,
+                fit: FlexFit.tight,
+                child: Text(
+                  order.menuList.first.name,
+                  textAlign: TextAlign.center,
+                ),
               ),
-            ),
-            Flexible(
-              flex: 1,
-              fit: FlexFit.tight,
-              child: Text(
-                order.status.name,
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                    color: order.status.name == OrderStatus.cancelled.name
-                        ? Colors.red
-                        : Colors.green),
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Text(
+                  order.status.name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: order.status.name == OrderStatus.cancelled.name
+                          ? Colors.red
+                          : Colors.green),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
