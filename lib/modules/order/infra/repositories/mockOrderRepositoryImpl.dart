4258521dbcb34196a9a6023a4e20cf58707456f6
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-15 21:07:07 -0300

    Fixing wrong methods

--- lib/modules/order/infra/repositories/mockOrderRepositoryImpl.dart
+++ lib/modules/order/infra/repositories/mockOrderRepositoryImpl.dart
@@ -7,18 +7,19 @@ import '../../domain/entities/order.dart' as order;
 
 class MockOrderRepositoryImpl implements IOrderRepository {
   @override
-  Future<Either<OrderError, order.Order>> cancel(order.Order order) async {
+  Future<Either<OrderError, order.Order>> create(order.Order order) async {
     await Future.delayed(const Duration(seconds: 2));
 
     return Right(order);
   }
 
   @override
-  Future<Either<OrderError, order.Order>> create(order.Order order) async {
+  Future<Either<OrderError, order.Order>> cancel(order.Order order) async {
     await Future.delayed(const Duration(seconds: 2));
 
     order.status = OrderStatus.cancelled;
 
+    //return Left(OrderError('Order Expired'));
     return Right(order);
   }
 }
