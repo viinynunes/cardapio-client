2e810b9d91c13a90099fe3a68a2409b01331e68a
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-19 16:19:39 -0300

    HiveDatasource to offline data

---
+++ lib/modules/login/external/impl/hive_datasource.dart
@@ -0,0 +1,25 @@
+import 'package:cardapio/modules/login/domain/entities/user.dart';
+import 'package:cardapio/modules/login/infra/datasources/i_logged_user_datasource.dart';
+import 'package:cardapio/modules/login/infra/models/user_model.dart';
+
+class HiveDatasource implements ILoggedUserDatasource {
+  @override
+  Future<User> getLoggedUser() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    return User(
+        name: 'Vinicius Nunes', email: 'viny@gmail.com', phone: '19981436342');
+
+    /*return Left(LoginError('Usuario ou senha invalido'));*/
+  }
+
+  @override
+  Future<bool> logout() async {
+    return true;
+  }
+
+  @override
+  Future<UserModel> saveLoggedUser(UserModel user) async {
+    return user;
+  }
+}
