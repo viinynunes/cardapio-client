82ffde8561d39348583c841bb0ddfddb64130c3e
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-19 16:23:23 -0300

    Getting correct Binds

--- lib/modules/login/login_module.dart
+++ lib/modules/login/login_module.dart
@@ -1,15 +1,19 @@
+import 'package:cardapio/modules/login/domain/usecases/impl/login_usecase_impl.dart';
+import 'package:cardapio/modules/login/external/impl/firebase_datasource.dart';
+import 'package:cardapio/modules/login/external/impl/hive_datasource.dart';
+import 'package:cardapio/modules/login/infra/repositories/login_repository_impl.dart';
 import 'package:cardapio/modules/login/presenter/bloc/login_bloc.dart';
 import 'package:cardapio/modules/login/presenter/pages/login_page.dart';
-import 'package:cardapio/modules/login/domain/repositories/mock_login_repository.dart';
-import 'package:cardapio/modules/login/domain/usecases/impl/login_usecase_impl.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class LoginModule extends Module {
   @override
   List<Bind> get binds => [
-        Bind.factory((i) => MockLoginRepository()),
-        Bind.factory((i) => LoginUsecaseImpl(i())),
-        Bind.singleton((i) => LoginBloc(i()))
+        Bind((i) => HiveDatasource()),
+        Bind((i) => FirebaseDatasource(i())),
+        Bind((i) => LoginRepositoryImpl(i())),
+        Bind((i) => LoginUsecaseImpl(i())),
+        Bind((i) => LoginBloc(i()))
       ];
 
   @override
