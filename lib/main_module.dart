bc1337b44e26cc498711805a75190c4a177b7c41
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-19 17:07:52 -0300

    main_module.dart handling with login page

--- lib/main_module.dart
+++ lib/main_module.dart
@@ -1,21 +1,32 @@
 import 'package:cardapio/modules/cart/cart_module.dart';
-import 'package:cardapio/modules/login/login_module.dart';
-import 'package:cardapio/modules/order/order_module.dart';
-import 'package:cardapio/modules/menu/menu_module.dart';
 import 'package:cardapio/modules/home/presenter/pages/home_page.dart';
+import 'package:cardapio/modules/login/domain/usecases/impl/login_usecase_impl.dart';
+import 'package:cardapio/modules/login/external/impl/firebase_datasource.dart';
+import 'package:cardapio/modules/login/external/impl/hive_datasource.dart';
+import 'package:cardapio/modules/login/infra/repositories/login_repository_impl.dart';
+import 'package:cardapio/modules/login/presenter/bloc/login_bloc.dart';
+import 'package:cardapio/modules/login/presenter/pages/login_page.dart';
+import 'package:cardapio/modules/menu/menu_module.dart';
+import 'package:cardapio/modules/order/order_module.dart';
 import 'package:cardapio/modules/splash/splash_screen.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class MainModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind((i) => HiveDatasource()),
+        Bind((i) => FirebaseDatasource(i())),
+        Bind((i) => LoginRepositoryImpl(i())),
+        Bind((i) => LoginUsecaseImpl(i())),
+        Bind((i) => LoginBloc(i()))
+      ];
 
   @override
   List<ModularRoute> get routes => [
         ChildRoute('/', child: (_, __) => const SplashScreenInitial()),
-        ChildRoute('/home', child: (_, __) => const HomePage()),
-        ModuleRoute('/login/', module: LoginModule()),
-        ModuleRoute('/menu', module: MenuModule()),
+        ChildRoute('/home/', child: (_, __) => const HomePage()),
+        ChildRoute('/login/', child: (_, __) => const LoginPage()),
+        ModuleRoute('/menu/', module: MenuModule()),
         ModuleRoute('/order/', module: OrderModule()),
         ModuleRoute('/cart/', module: CartModule()),
       ];
