35924eb235136407ba067a55fed6360694ee5fcf
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-19 11:25:58 -0300

    Added Firebase support

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:cardapio/main_app.dart';
 import 'package:cardapio/main_module.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   Modular.to.addListener(() {
     debugPrint(Modular.to.path);
   });
