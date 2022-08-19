4b894ade01deaa07b5186df0121b8a0ff320d9f8
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-19 16:20:17 -0300

    Added saveLoggedUser contract to get online user and store offiline

--- lib/modules/login/infra/datasources/i_logged_user_datasource.dart
+++ lib/modules/login/infra/datasources/i_logged_user_datasource.dart
@@ -1,7 +1,11 @@
+import 'package:cardapio/modules/login/infra/models/user_model.dart';
+
 import '../../domain/entities/user.dart';
 
 abstract class ILoggedUserDatasource {
   Future<User> getLoggedUser();
+
+  Future<UserModel> saveLoggedUser(UserModel user);
 
   Future<bool> logout();
 }
