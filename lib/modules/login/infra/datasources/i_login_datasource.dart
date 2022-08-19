2e8918d36237df69853e9997f6e24cf37819f288
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-19 11:31:43 -0300

    Removed getLoggedUser function

--- lib/modules/login/infra/datasources/i_login_datasource.dart
+++ lib/modules/login/infra/datasources/i_login_datasource.dart
@@ -1,8 +1,5 @@
 import 'package:cardapio/modules/login/infra/models/user_model.dart';
 
 abstract class ILoginDatasource {
-
   Future<UserModel> login(String email, String password);
-
-  Future<UserModel> getLoggedUser();
 }
