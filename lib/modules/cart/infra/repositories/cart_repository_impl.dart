6f5909de3800968f90732e771fddbf7fe32c8d14
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-21 21:21:47 -0300

    Implementation of ICartRepository

---
+++ lib/modules/cart/infra/repositories/cart_repository_impl.dart
@@ -0,0 +1,64 @@
+import 'package:cardapio/modules/cart/domain/repositories/i_cart_repository.dart';
+import 'package:cardapio/modules/cart/infra/models/item_menu_model.dart';
+import 'package:cardapio/modules/errors/errors.dart';
+import 'package:cardapio/modules/menu/domain/entities/item_menu.dart';
+import 'package:dartz/dartz.dart';
+
+import '../datasources/i_cart_datasource.dart';
+
+class CartRepositoryImpl implements ICartRepository {
+  final ICartDatasource _cartDatasource;
+
+  CartRepositoryImpl(this._cartDatasource);
+
+  @override
+  Future<Either<CartError, ItemMenu>> addItemToCart(ItemMenu itemMenu) async {
+    try {
+      final result = await _cartDatasource
+          .addItemToCart(ItemMenuModel.fromItemMenu(itemMenu: itemMenu));
+
+      return Right(result);
+    } on CartError catch (e) {
+      return Left(e);
+    } catch (e) {
+      return Left(CartError(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<CartError, bool>> removeItemFromCart(ItemMenu itemMenu) async {
+    try {
+      final result =
+          await _cartDatasource.removeItemFromCart(itemMenu as ItemMenuModel);
+      return Right(result);
+    } on CartError catch (e) {
+      return Left(e);
+    } catch (e) {
+      return Left(CartError(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<CartError, bool>> clearMenuCartList() async {
+    try {
+      final result = await _cartDatasource.clearMenuCartList();
+      return Right(result);
+    } on CartError catch (e) {
+      return Left(e);
+    } catch (e) {
+      return Left(CartError(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<CartError, List<ItemMenu>>> getMenuCartList() async {
+    try {
+      final result = await _cartDatasource.getMenuCartList();
+      return Right(result);
+    } on CartError catch (e) {
+      return Left(e);
+    } catch (e) {
+      return Left(CartError(e.toString()));
+    }
+  }
+}
