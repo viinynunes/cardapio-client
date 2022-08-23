de95c500d3e9a2bfbb6cc48aa320e16fc4009cf3
Vinicius Nunes <viiny_nunes@hotmail.com>
2022-08-23 13:49:29 -0300

    Removed Mock dependency and put correct implementations

--- lib/modules/binds_and_routes/login/navigation/navigation_module.dart
+++ lib/modules/binds_and_routes/login/navigation/navigation_module.dart
@@ -1,5 +1,5 @@
 import 'package:cardapio/modules/cart/infra/repositories/cart_repository_impl.dart';
-import 'package:cardapio/modules/order/infra/repositories/mockOrderRepositoryImpl.dart';
+import 'package:cardapio/modules/order/infra/repositories/order_repository_impl.dart';
 import 'package:cardapio/modules/splash/splash_screen.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -8,6 +8,7 @@ import '../../../cart/external/datasources/impl/cart_firebase_datasource.dart';
 import '../../../cart/presenter/bloc/cart_bloc.dart';
 import '../../../home/presenter/pages/home_page.dart';
 import '../../../order/domain/usecases/impl/order_usecase_impl.dart';
+import '../../../order/external/datasources/impl/order_firebase_datasource.dart';
 import '../../../order/presenter/bloc/order_bloc.dart';
 import 'cart/cart_module.dart';
 import 'menu/menu_module.dart';
@@ -23,7 +24,8 @@ class NavigationModule extends Module {
         Bind((i) => CartBloc(i())),
 
         //Order Dependencies
-        Bind((i) => MockOrderRepositoryImpl()),
+        Bind((i) => OrderFirebaseDatasource(i())),
+        Bind((i) => OrderRepositoryImpl(i())),
         Bind((i) => OrderUsecaseImpl(i())),
         Bind((i) => OrderBloc(i(), i(), i(), i()))
       ];
